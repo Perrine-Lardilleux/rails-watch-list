bfbeac102206bad9a0feb14c227b60263f15d788
Perrine Lardilleux <perrinelardilleux@gmail.com>
2021-05-28 16:53:23 -0300

    site cinema

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -14,14 +14,16 @@ class ListsController < ApplicationController
 
   def create
     @list = List.new(list_params)
-    @list.save
-    redirect_to list_path(@list)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
   end
 
   private
 
   def list_params
-    params.require(:list).permit(:name)
-    params.require(:list).permit(:title, :body, :photo)
+    params.require(:list).permit(:name, :photo)
   end
 end
